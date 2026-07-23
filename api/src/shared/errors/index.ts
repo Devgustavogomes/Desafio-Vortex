0afde89bcd194271df8e3fd233865415a72730dd
Devgustavogomes <devgustavogomes31@gmail.com>
2026-07-23 13:53:42 -0300

    feat: create custom errors

---
+++ api/src/shared/errors/index.ts
@@ -0,0 +1,6 @@
+export { BaseError } from './BaseError';
+export { NotFoundError } from './NotFoundError';
+export { UnauthorizedError } from './UnauthorizedError';
+export { ForbiddenError } from './ForbiddenError';
+export { ConflictError } from './ConflictError';
+export { ValidationError } from './ValidationError';
